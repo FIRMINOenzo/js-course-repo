2ffb6a8bd7a77b9997fb7b479e9dc68a9d0f0dea
Enzo Firmino Campanari <enzofirmino.camp@gmail.com>
2023-03-14 20:50:37 -0300

    initial import

---
+++ easy-http/easy-http3.js
@@ -0,0 +1,56 @@
+/**
+ * @version 3.0.0
+ * @author Enzo Firmino Campanari
+ * @license MIT
+ */
+
+class EasyHTTP {
+  async get(url) {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    return data;
+  }
+
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const datas = await response.json();
+
+    return datas;
+  }
+
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const datas = await response.json();
+
+    return datas;
+  }
+
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "delete",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    return data;
+  }
+}
