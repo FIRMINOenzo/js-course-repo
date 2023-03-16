eeea3e49f3fa39806ba1cc784b76ebf96a328f09
Enzo Firmino Campanari <114703471+FIRMINOenzo@users.noreply.github.com>
2023-03-15 22:17:41 -0300

    initial import

--- github-finder/github.js
+++ github-finder/github.js
@@ -1,18 +1,16 @@
 class GitHub {
   constructor() {
-    this.client_id = "3a3875b7edb93d8f12a5";
-    this.client_secret = "34fa47ea2eefd94e9c25f7f0c1b70427883ddee4";
     this.repos_count = 5;
     this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}`
     );
 
     const reposResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`
     );
 
     const profile = await profileResponse.json(),
